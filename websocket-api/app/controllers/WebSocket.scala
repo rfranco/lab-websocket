dcf5be461a9043f749dcd30d2973614d82168af3
Ricardo Franco <rmf.franco@gmail.com>
2014-12-12 15:19:27 -0200

    add timer

--- websocket-api/app/controllers/WebSocket.scala
+++ websocket-api/app/controllers/WebSocket.scala
@@ -1,9 +1,11 @@
 package controllers
 
 import akka.actor._
+import org.joda.time.DateTime
 import play.api.Logger
 import play.api.Play.current
 import play.api.mvc._
+import scala.concurrent.duration._
 
 object WebSocketApi extends Controller {
 
@@ -14,12 +16,46 @@ object WebSocketApi extends Controller {
 
 object WebSocketActor {
   def props(out: ActorRef) = Props(new WebSocketActor(out))
+  sealed trait SocketMessage
+  case object UpdateTime extends SocketMessage
 }
 
 class WebSocketActor(out: ActorRef) extends Actor {
+
+  import WebSocketActor._
+  import context.dispatcher
+  Logger.info(s"Init: ${self.path}")
+  val cancellable = context.system.scheduler.schedule(0.second, 5.seconds, self, UpdateTime)
+
   def receive = {
     case msg: String =>
       Logger.info(s"Receive: $msg")
-      out ! ("I received your message: " + msg)
+      out ! s"I received your message: $msg"
+    case UpdateTime =>
+      out ! s"Clock ${DateTime.now}"
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def preStart(): Unit = {
+    Logger.info(s"preStart: ${self.path}")
+    super.preStart()
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def postStop(): Unit = {
+    Logger.info(s"postStop: ${self.path}")
+    super.postStop()
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def preRestart(reason: Throwable, message: Option[Any]): Unit = {
+    Logger.info(s"preRestart: ${self.path}")
+    super.preRestart(reason, message)
+  }
+
+  @throws[Exception](classOf[Exception])
+  override def postRestart(reason: Throwable): Unit = {
+    Logger.info(s"postRestart: ${self.path}")
+    super.postRestart(reason)
   }
 }
